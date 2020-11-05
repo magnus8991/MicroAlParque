4cc7b761c2af627e32604634b4c6a4d761866e6f
magnus8991 <>
2020-11-05 00:35:21 -0500

    respuesta

--- Logica/Respuesta.cs
+++ Logica/Respuesta.cs
@@ -1,7 +1,33 @@
+using System.Collections.Generic;
+
 namespace Logica
 {
-    public class Respuesta
+    public class Respuesta<G>
     {
-        
+        public G Elemento { get; set; }
+        public string Mensaje { get; set; }
+        public bool Error { get; set; }
+
+        public Respuesta(G elemento, string mensaje, bool error)
+        {
+            Elemento = elemento;
+            Mensaje = mensaje;
+            Error = error;
+        }
+        public Respuesta() { }
+    }
+
+    public class RespuestaConsulta<G>
+    {
+        public IList<G> Elementos { get; set; }
+        public string Mensaje { get; set; }
+        public bool Error { get; set; }
+
+        public RespuestaConsulta(IList<G> elementos, string mensaje, bool error) {
+            Elementos = elementos;
+            Mensaje = mensaje;
+            Error = error;
+        }
+        public RespuestaConsulta() { }
     }
 }
