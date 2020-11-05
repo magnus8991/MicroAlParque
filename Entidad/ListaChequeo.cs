2e75fe1a01cca55449ac7a91245dbcee14826cea
magnus8991 <dalfredomandon@unicesar.edu.co>
2020-11-05 09:01:55 -0500

    :V

--- Entidad/ListaChequeo.cs
+++ Entidad/ListaChequeo.cs
@@ -11,12 +11,8 @@ namespace Entidad
         public int ListaChequeoId { get; set; }
         public IList<RespuestaChequeo> RespuestaChequeos { get; set; }
         public int RestauranteId { get; set; }
-<<<<<<< HEAD
-        public ListaChequeo()
-        {
-=======
+        
         public ListaChequeo() {
->>>>>>> 407ce48... modelos y controllers
             RespuestaChequeos = new List<RespuestaChequeo>();
         }
     }
