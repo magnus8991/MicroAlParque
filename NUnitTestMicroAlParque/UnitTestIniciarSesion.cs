1a8a094e5a56e1c77c406a307b3c45c99b6cb367
diego andres villa cardona <spaydiego@hotmail.com>
2021-03-31 21:08:33 -0500

    Cambios Prueba

---
+++ NUnitTestMicroAlParque/UnitTestIniciarSesion.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NUnitTestMicroAlParque
+{
+    public class UnitTestIniciarSesion
+    {
+        public IWebDriver webDriver { get; set; }
+        [SetUp]
+        public void Setup()
+        {
+            webDriver = new ChromeDriver(@"D:\Descargas\chromedriver");
+            webDriver.Navigate().GoToUrl("https://localhost:44376/");
+        }
+
+        [TestCase("","")]
+        [Test]
+        public void inicioSesionTest(string nombreUsuario, string contraseña)
+        {
+            IWebElement inputUsuario = webDriver.FindElement(By.Name("nombreUsuario"));
+            IWebElement inputConstrasena = webDriver.FindElement(By.Name("contrasena"));
+            inputUsuario.SendKeys(nombreUsuario);
+            inputConstrasena.SendKeys(contraseña);
+            IWebElement btnIniciarSesion = webDriver.FindElement(By.Name("btnIniciarSesion"));
+
+            Assert.Pass();
+        }
+    }
+}
